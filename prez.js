97c029c0a5d424e4c172accc314669d38545f30a
CryptoThibault <thibault.chalaoux@gmail.com>
2021-02-12 14:48:02 +0100

    Update

--- prez.js
+++ prez.js
@@ -1,1 +1,4 @@
-console.log('Hello')
+let prename = 'Sofiane'
+let age = '39'
+let city = 'Paris'
+console.log('Hello, my name is ' + prename + ' and i am ' + age + ' years old. I live in ' + city + '.')
---
+++ pyramid_generic.js
@@ -0,0 +1,10 @@
+let a = 'A'
+let s = ' '
+console.log(a + s + s + s + s + s + s + s + s + s + s + s + s + s + s + a)
+console.log(a + a + s + s + s + s + s + s + s + s + s + s + s + s + a + a)
+console.log(a + a + a + s + s + s + s + s + s + s + s + s + s + a + a + a)
+console.log(a + a + a + a + s + s + s + s + s + s + s + s + a + a + a + a)
+console.log(a + a + a + a + a + s + s + s + s + s + s + a + a + a + a + a)
+console.log(a + a + a + a + a + a + s + s + s + s + a + a + a + a + a + a)
+console.log(a + a + a + a + a + a + a + s + s + a + a + a + a + a + a + a)
+console.log(a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a)
